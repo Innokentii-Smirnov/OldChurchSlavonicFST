de28f144708e5be1287bfe505cbc6535b89cdc5d
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-31 21:43:07 +0300

    Move out FST compilation into a submodule

--- test.sh
+++ test.sh
@@ -1,10 +1,7 @@
 filepath="Segmentation/Segmentation.foma"
-directory=$(dirname $filepath)
-name=$(basename $filepath .foma)
-binary=$name.bin
-cd $directory
-foma -e "source $name.foma" -e "push $name" -e "save stack $binary" -e "sigma" -e "exit"
-echo "Created Foma binary $binary."
-cd ..
+directory=$(dirname "$filepath")
+name=$(basename "$filepath" .foma)
+binary="$name.bin"
+CompileFST/CompileFST.sh "$filepath"
 tr -d '12/ *' < input.txt | flookup -s " ← " -b "$directory/$binary" > output.txt
 python evaluate.py
