25e04057fbc39c9416f5ba2d306b6789cc7a4637
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-02 20:22:06 +0300

    Move FST application to the test file (= the flookup call)  to the CompileFST submodule

--- test.sh
+++ test.sh
@@ -1,7 +1,3 @@
 filepath="src/Segmentation/Segmentation.foma"
-directory=$(dirname "$filepath")
-name=$(basename "$filepath" .foma)
-binary="$name.bin"
 CompileFST/CompileFST.sh "$filepath"
-flookup -s " ← " -b "$directory/$binary" < input.txt > output.txt
 python evaluate.py
